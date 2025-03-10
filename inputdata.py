f33648358905bcf37dc76c5306e950987f3427c5
ijeny <imtinan.jmb6628@gmail.com>
2025-03-10 16:58:12 +0700

    Add files via upload

---
+++ inputdata.py
@@ -0,0 +1,9 @@
+# input 
+barang = input('nama barang : ')
+jumlah = input('jumlah barang : ')
+harga = input('harga barang : Rp. ')
+
+# output
+print('barang yang anda beli',barang)
+print('sejumlah',jumlah)
+print('dengan harga',harga)
---
+++ variabel.py
@@ -0,0 +1,9 @@
+# buat 2 variabel, isi dengan angka
+jeruk = 14
+apel = 6
+
+# hitung total jumlahnya
+TotalBuah= jeruk + apel
+
+# tampilkan hasil
+print(TotalBuah)
