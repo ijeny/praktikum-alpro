6379a649a9fe97d52822797ffb9151a599649dcc
ijeny <imtinan.jmb6628@gmail.com>
2025-03-10 17:02:57 +0700

    Add files via upload

---
+++ tipedata.py
@@ -0,0 +1,23 @@
+# string : text
+nama = 'seiji'
+# integer : bilangan bulat 
+umur = 25
+# float : bilangan desimal 
+IPK = 3.8
+# list : multiple data 
+barang = ['tas','buku','laptop']
+# tuple : multiple data
+buah = ('jeruk','apel','mangga')
+# set : multiple data
+matkul = {'alpro','desain','instalasi'}
+# dictionary : multiple data 
+dataSiswa = {'nama':'seiji','umur':'25'}
+
+# tampilkan tipe data 
+print('data 1 tipe datanya :',type(nama))
+print('data 2 tipe datanya :',type(umur))
+print('data 3 tipe datanya :',type(IPK))
+print('data 4 tipe datanya :',type(barang))
+print('data 5 tipe datanya :',type(buah))
+print('data 6 tipe datanya :',type(matkul))
+print('data 7 tipe datanya :',type(dataSiswa))
