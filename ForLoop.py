01dd4108ade159213eef3ee03a6c70a1ce067476
ijeny <imtinan.jmb6628@gmail.com>
2025-03-30 15:43:08 +0700

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,12 @@
+# contoh 1
+print('FOR IN SEQUENCE')
+nama = ['Luna','Helen','Rijal','Frio','Dheja']
+for nama in nama :
+    print('Nama :',nama)
+print('----------------')
+
+# contoh 2
+print('FOR IN RANGE')
+for nilai in range(5,20) :
+    print('pengulangan ke :',nilai)
+print('--------------------------')
---
+++ WhileLoop.py
@@ -0,0 +1,11 @@
+nilaiAwal = 4
+while nilaiAwal <= 1000 :
+    print('pengulangan ke :',nilaiAwal)
+    nilaiAwal += 4
+    #nilaiAwal = nilaiAwal + 4
+
+NilaiAwal = 30
+while NilaiAwal >= 1 :
+    print('hitung mundur :',NilaiAwal)
+    NilaiAwal -= 1
+    #NilaiAwal = NilaiAwal - 1
